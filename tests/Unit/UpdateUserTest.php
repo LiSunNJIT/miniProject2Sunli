afc778a80946f05463cceab83a6708c378f7a2e9
li <ls447@njit.edu>
2019-05-12 02:41:43 -0400

    6.  Create a unit test to update the name of a user in the database to Steve Smith.

---
+++ tests/Unit/UpdateUserTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UpdateUserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+
+        {
+            $user = new user;
+
+            $user-> name='Steve Smith';
+            $user-> email='stevesmith888@njit.edu';
+            $user-> password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
+
+        }
+
+
+    }
+}
